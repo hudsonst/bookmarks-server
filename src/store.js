18ff4d2375fab7c488b37b980b30edd7ebe9dc57
hudsonst <nadinegraham@gmail.com>
2019-12-31 00:36:32 -0500

    Initial Revision

---
+++ src/store.js
@@ -0,0 +1,9 @@
+const bookmarks = [ {
+    id: 1,
+    title: 'Arie Kaplan',
+    url: 'www.ariekaplan.com',
+    description: 'Writer\'s website',
+    rating: '5'
+}]
+
+module.exports = { bookmarks }
